04bb2b14a08aa0d3ac8856c4b839e5cc281582e8
PunRabbit <tjdmsch2201@gmail.com>
2024-05-27 23:11:17 +0900

    Add wait-for-it for Dockerfile and Add setup.py for test env.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='python-fastapi-template',
+    version='0.1',
+    packages=find_packages(where='src'),
+    description='for test code dependencies'
+)
